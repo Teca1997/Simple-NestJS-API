206679277ac709e1e2ed49ef83355adfbc4e3f2e
Teca <57445519+Teca1997@users.noreply.github.com>
2023-01-31 14:41:26 +0100

    re init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
-import { RolesModule } from './roles/roles.module';
-import { TokensModule } from './tokens/tokens.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, RolesModule, TokensModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
-import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import { greenBright } from 'colorette';
-
-const port = parseInt(process.env.PORT || '3000', 10);
-const dev = process.env.NODE_ENV !== 'production';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log(
-    greenBright(
-      `Server listening at http://localhost:${port} as ${
-        dev ? 'development' : process.env.NODE_ENV
-      }`,
-    ),
-  );
 }
 bootstrap();
