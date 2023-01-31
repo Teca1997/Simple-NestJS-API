8f4ff48855f7037670a58d88049f22602682ab1c
Teca <57445519+Teca1997@users.noreply.github.com>
2023-01-31 15:00:43 +0100

    added user, token and role resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { Role } from './roles/entities/role.entity';
+import { RolesModule } from './roles/roles.module';
+import { Token } from './tokens/entities/token.entity';
+import { TokensModule } from './tokens/tokens.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    RolesModule,
+    TokensModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      database: process.env.DB_DATABASE || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      schema: process.env.DB_SCHEMA || 'public',
+      migrationsRun: false,
+      synchronize: false,
+      dropSchema: false,
+      entities: [User, Role, Token],
+      subscribers: [],
+      logger: 'advanced-console',
+      poolSize: 15,
+      logging: false /* process.env.DB_LOGGING === "true" */,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { greenBright } from 'colorette';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  console.log(
+    greenBright(
+      `Server listening at http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`,
+    ),
+  );
 }
 bootstrap();
