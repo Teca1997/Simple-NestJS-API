84b3c4c8aeaf81a70b0c392f470a2d87d8e7c193
Teca <57445519+Teca1997@users.noreply.github.com>
2023-02-02 21:34:28 +0100

    added webpack HMR

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,15 @@ import { greenBright } from 'colorette';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Product management API example')
+    .setTitle('Product management API')
     .setDescription('The product management API description')
     .setVersion('1.0')
     .build();
@@ -21,6 +23,11 @@ async function bootstrap() {
   SwaggerModule.setup('', app, document);
 
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   console.log(greenBright(`Server listening at http://localhost:${port} as ${process.env.NODE_ENV}`));
 }
